6a5781163ccaac5e4bba0f60f456f4914c9d9506
kiey <joan.marcual.medina@gmail.com>
2020-09-15 11:25:04 +0200

    Ejercicio mostrar datos del formulario en /añadir

--- capitulo_7/listas/views.py
+++ capitulo_7/listas/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import redirect, render
-
+from django.http import HttpResponse
 # Create your views here.
 def vista_principal(request):
     return render(request, 'home.html')
 def vista_anadir(request):
+    prioridad = request.POST["name_prioridad"]
+    titulo = request.POST["nombre_titulo"]
+    nota = request.POST["name_nota"]
+    return(HttpResponse(f'Insertado <br> '
+                        f'prioridad: {prioridad}<br>'
+                        f'titulo: {titulo}<br>'
+                        f'nota: {nota}' ))
+
+    """
+    
+    :param request: 
+    :return: 
     Item.objects.create(text=request.POST['item_text'], list=list_)
 
     conn = psycopg2.connect(dbname="capitulo_4_db",
@@ -18,3 +30,4 @@ def vista_anadir(request):
     conn.close()
     return HttpResponse("Insertado")
     return redirect('home')
+    """

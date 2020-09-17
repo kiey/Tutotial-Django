90154c71c35d4cb211382076ef145f7c62c76c38
kiey <joan.marcual.medina@gmail.com>
2020-09-17 17:40:05 +0200

    Capitulo 6 finsihed

--- capitulo_6/listas/views.py
+++ capitulo_6/listas/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 import psycopg2
+import psycopg2.extras
+
 # Create your views here.
 def vista_principal(request):
-    return render(request, 'home.html')
+    conn = psycopg2.connect(dbname="capitulo_6_db",
+                            user="capitulo_6_user",
+                            password="patata")
+    cursor = conn.cursor()
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM Nota;")
+    result = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    params = {'notas': result}
+    return render(request, 'home.html', params)
+
+
+def vista_principal_(request):
+    conn = psycopg2.connect(dbname="capitulo_6_db",
+                            user="capitulo_6_user",
+                            password="patata")
+
+    cursor = conn.cursor()
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cursor.execute("SELECT * FROM Nota;")
+    result = cursor.fetchone()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    params = {'nota': result}
+    return render(request, 'home.html', params)
+
 def vista_anadir(request):
-    
+
     prioridad = request.POST["name_prioridad"]
     titulo = request.POST["nombre_titulo"]
     nota = request.POST["name_nota"]
